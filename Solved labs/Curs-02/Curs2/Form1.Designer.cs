44dfa58af9f5bda7886d69ad3337b173fa44bdb5
natabobu <61291322+natabobu@users.noreply.github.com>
2020-10-06 19:32:56 +0300

    Add files via upload

---
+++ Solved labs/Curs-02/Curs2/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Curs2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnActualizare = new System.Windows.Forms.Button();
+            this.btnSalvare = new System.Windows.Forms.Button();
+            this.btnRenuntare = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idPersoanaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cNPDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.salariuDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idLocalitateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.spImagineDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.persoaneBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSet1 = new Curs2.DataSet1();
+            this.persoaneTableAdapter = new Curs2.DataSet1TableAdapters.PersoaneTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.persoaneBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnActualizare
+            // 
+            this.btnActualizare.Location = new System.Drawing.Point(67, 37);
+            this.btnActualizare.Name = "btnActualizare";
+            this.btnActualizare.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizare.TabIndex = 0;
+            this.btnActualizare.Text = "Actualizare";
+            this.btnActualizare.UseVisualStyleBackColor = true;
+            this.btnActualizare.Click += new System.EventHandler(this.btnActualizare_Click);
+            // 
+            // btnSalvare
+            // 
+            this.btnSalvare.Location = new System.Drawing.Point(161, 37);
+            this.btnSalvare.Name = "btnSalvare";
+            this.btnSalvare.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvare.TabIndex = 1;
+            this.btnSalvare.Text = "Salvare";
+            this.btnSalvare.UseVisualStyleBackColor = true;
+            this.btnSalvare.Click += new System.EventHandler(this.btnSalvare_Click);
+            // 
+            // btnRenuntare
+            // 
+            this.btnRenuntare.Location = new System.Drawing.Point(255, 37);
+            this.btnRenuntare.Name = "btnRenuntare";
+            this.btnRenuntare.Size = new System.Drawing.Size(75, 23);
+            this.btnRenuntare.TabIndex = 2;
+            this.btnRenuntare.Text = "Renuntare";
+            this.btnRenuntare.UseVisualStyleBackColor = true;
+            this.btnRenuntare.Click += new System.EventHandler(this.btnRenuntare_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idPersoanaDataGridViewTextBoxColumn,
+            this.numeDataGridViewTextBoxColumn,
+            this.cNPDataGridViewTextBoxColumn,
+            this.salariuDataGridViewTextBoxColumn,
+            this.idLocalitateDataGridViewTextBoxColumn,
+            this.spImagineDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.persoaneBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 75);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(646, 150);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.dataGridView1_DataError);
+            this.dataGridView1.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.dataGridView1_UserDeletingRow);
+            // 
+            // idPersoanaDataGridViewTextBoxColumn
+            // 
+            this.idPersoanaDataGridViewTextBoxColumn.DataPropertyName = "IdPersoana";
+            this.idPersoanaDataGridViewTextBoxColumn.HeaderText = "IdPersoana";
+            this.idPersoanaDataGridViewTextBoxColumn.Name = "idPersoanaDataGridViewTextBoxColumn";
+            // 
+            // numeDataGridViewTextBoxColumn
+            // 
+            this.numeDataGridViewTextBoxColumn.DataPropertyName = "Nume";
+            this.numeDataGridViewTextBoxColumn.HeaderText = "Nume";
+            this.numeDataGridViewTextBoxColumn.Name = "numeDataGridViewTextBoxColumn";
+            // 
+            // cNPDataGridViewTextBoxColumn
+            // 
+            this.cNPDataGridViewTextBoxColumn.DataPropertyName = "CNP";
+            this.cNPDataGridViewTextBoxColumn.HeaderText = "CNP";
+            this.cNPDataGridViewTextBoxColumn.Name = "cNPDataGridViewTextBoxColumn";
+            // 
+            // salariuDataGridViewTextBoxColumn
+            // 
+            this.salariuDataGridViewTextBoxColumn.DataPropertyName = "Salariu";
+            this.salariuDataGridViewTextBoxColumn.HeaderText = "Salariu";
+            this.salariuDataGridViewTextBoxColumn.Name = "salariuDataGridViewTextBoxColumn";
+            // 
+            // idLocalitateDataGridViewTextBoxColumn
+            // 
+            this.idLocalitateDataGridViewTextBoxColumn.DataPropertyName = "IdLocalitate";
+            this.idLocalitateDataGridViewTextBoxColumn.HeaderText = "IdLocalitate";
+            this.idLocalitateDataGridViewTextBoxColumn.Name = "idLocalitateDataGridViewTextBoxColumn";
+            // 
+            // spImagineDataGridViewTextBoxColumn
+            // 
+            this.spImagineDataGridViewTextBoxColumn.DataPropertyName = "SpImagine";
+            this.spImagineDataGridViewTextBoxColumn.HeaderText = "SpImagine";
+            this.spImagineDataGridViewTextBoxColumn.Name = "spImagineDataGridViewTextBoxColumn";
+            // 
+            // persoaneBindingSource
+            // 
+            this.persoaneBindingSource.DataMember = "Persoane";
+            this.persoaneBindingSource.DataSource = this.dataSet1;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // persoaneTableAdapter
+            // 
+            this.persoaneTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 262);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnRenuntare);
+            this.Controls.Add(this.btnSalvare);
+            this.Controls.Add(this.btnActualizare);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.persoaneBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnActualizare;
+        private System.Windows.Forms.Button btnSalvare;
+        private System.Windows.Forms.Button btnRenuntare;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource persoaneBindingSource;
+        private DataSet1TableAdapters.PersoaneTableAdapter persoaneTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idPersoanaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cNPDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn salariuDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idLocalitateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn spImagineDataGridViewTextBoxColumn;
+    }
+}
+
